d5f95dc280fe322e3775b38fdd1a0948348b8f35
Juhwan <ptagwhite@gmail.com>
2021-10-29 20:59:20 +0900

    JS Study (dream)
    ~3

---
+++ javascript-dream/2/main.js
@@ -0,0 +1,5 @@
+// JavaScript is very flexible
+// flexible === dangerous
+// vanilla js를 사용할 때 'use strict' 사용!
+'use strict';
+console.log('Hello World!');
---
+++ javascript-dream/3/variable.js
@@ -0,0 +1,110 @@
+// 1.Use strict
+// added in ES 5
+// use this for Vanilla Javascript
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+let globalName = 'global name'; {
+    let name = 'ellie';
+    console.log(name);
+    name = 'hello';
+    console.log(name);
+}
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// 호이스팅은 어디서 선언하든 항상 위로 끌어 올려준다.
+// has no block scope
+{
+    console.log(age);
+    age = 4;
+    console.log(age);
+    var age;
+}
+console.log(age);
+
+// 3. Constants
+// favor immutable data type always for a few reasons:
+// - security
+// - thread safety
+// -reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+// primitive, single item: number, string, boolean, null, undefined
+// object, box container
+// function, first-class function
+
+const count = 17; // integer
+const size = 17.1; // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number - special numeric values: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet)
+// 자동 스페이스바 때문인지 숫자 뒤에 n이 안들어간다.
+const bigInt = 1234567890123456789012345678901234567890; // over (-2**53) ~ 2**53
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+// String
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!` //template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+console.log('value: ' + helloBob + ', type: ' + typeof helloBob);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; //false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); // true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+// object, real-life object, data structure
+const ellie = { name: 'ellie', age: 20 };
+ellie.age = 21;
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); //h
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
+//runtime에서 type이 정해지기 때문에 오류가 뜬다.
+console.log(text.charAt(0));
