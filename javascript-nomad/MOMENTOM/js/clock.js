ff5d00bb34a8d16506d49fb362138a88b0526ac4
Juhwan <ptagwhite@gmail.com>
2021-11-04 05:47:56 +0900

    ~ #5.3 Recap

--- javascript-nomad/MOMENTOM/js/clock.js
+++ javascript-nomad/MOMENTOM/js/clock.js
@@ -1,8 +1,15 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-//setInterval(sayHello, 5000); // 시간마다 함수를 사용
-setTimeout(sayHello, 5000);
+// setInterval(sayHello, 5000); // 시간마다 함수를 사용
+// setTimeout(sayHello, 5000); // 시간이 흐른 뒤 함수를 사용
+
+getClock(); // 웹페이지가 열리면 즉시 함수가 실행되게
+setInterval(getClock, 1000);
