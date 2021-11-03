8a50d5bbb973ea78c76045f76439368e0a70d2c8
Juhwan <ptagwhite@gmail.com>
2021-11-04 00:21:54 +0900

    ~ # 3.7 CSS in Javascript part Two

--- javascript-nomad/MOMENTOM/app3.js
+++ javascript-nomad/MOMENTOM/app3.js
@@ -2,15 +2,12 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 
 function handleH1Click() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "tomato";
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        newColor = "blue";
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
-// h1.onclick = handleh1Click;과 같다.
 h1.addEventListener("click", handleH1Click);
