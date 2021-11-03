9d432839d28dfd85620f5bf78c98968af1aea8f7
Juhwan <ptagwhite@gmail.com>
2021-11-03 20:12:29 +0900

    ~ 2.12 returns
    make a simple calculator

--- javascript-nomad/MOMENTOM/app.js
+++ javascript-nomad/MOMENTOM/app.js
@@ -1,3 +1,36 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// calculator practice (211103)
 
-console.log(daysOfWeek[4]);
+const calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    minus: function(a, b) {
+        return a - b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    },
+    multiple: function(a, b) {
+        return a * b;
+    },
+    square: function(a, b) {
+        return a ** b;
+    },
+}
+
+const plusResult = calculator.add(12, 13);
+const minusResult = calculator.minus(plusResult, 10);
+const multipleResult = calculator.multiple(10, minusResult);
+const divideResult = calculator.divide(multipleResult, plusResult);
+const squareResult = calculator.square(divideResult, minusResult);
+
+console.log(squareResult);
+// const age = 96;
+
+// function calculateKrAge(ageOfForeigner) {
+//     return ageOfForeigner + 2;
+// };
+
+// const krAge = calculateKrAge(age);
+
+// console.log(krAge);
