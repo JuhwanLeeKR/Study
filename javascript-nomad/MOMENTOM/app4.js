1cb93e7862d5b9bd5b4f0cd207e8939d402718b6
Juhwan <ptagwhite@gmail.com>
2021-11-04 02:27:46 +0900

    ~ # 4.3 Events part Two
    preventDefault()
    addEventListener() mechanism

--- javascript-nomad/MOMENTOM/app4.js
+++ javascript-nomad/MOMENTOM/app4.js
@@ -1,9 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault(); // 어떤 event의 기본 행동이든지 발생되지 않도록 막는다.
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+    alert("clicked!");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
