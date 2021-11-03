269f2b937c5e24d80286b1f6d33fa731dcd8f905
Juhwan <ptagwhite@gmail.com>
2021-11-03 23:56:07 +0900

    ~ #3.5 More Events

--- javascript-nomad/MOMENTOM/app3.js
+++ javascript-nomad/MOMENTOM/app3.js
@@ -1,8 +1,43 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-function handleTitleClcik() {
-    title.style.color = "blue";
+
+console.dir(h1);
+
+function handleh1Click() {
+    h1.style.color = "blue";
 }
 
+function handleMouseEnter() {
+    h1.innerText = "Mouse is here!";
+}
 
-title.addEventListener("click", handleTitleClcik);
+function handleMouseLeave() {
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOD");
+}
+
+// h1.onclick = handleh1Click;과 같다.
+h1.addEventListener("click", handleh1Click);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
