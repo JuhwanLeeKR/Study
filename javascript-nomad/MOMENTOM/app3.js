e998171629ad10db2261eb9f08efbffe0bbef6db
Juhwan <ptagwhite@gmail.com>
2021-11-04 00:30:49 +0900

    ~ # 3.8 CSS in Javascript part Three

--- javascript-nomad/MOMENTOM/app3.js
+++ javascript-nomad/MOMENTOM/app3.js
@@ -2,12 +2,13 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 
 function handleH1Click() {
-    const clickedClass = "clicked";
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    // const clickedClass = "clicked";
+    // if (h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // } else {
+    //     h1.classList.add(clickedClass);
+    // .toggle()로 대체 가능.
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleH1Click);
