fb6fde346795399ab8b85ed73230628774ebae31
Juhwan <ptagwhite@gmail.com>
2021-11-04 00:27:31 +0900

    ~ # 3.7 CSS in Javascript part Two (Debug)

--- javascript-nomad/MOMENTOM/app3.js
+++ javascript-nomad/MOMENTOM/app3.js
@@ -3,10 +3,10 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleH1Click() {
     const clickedClass = "clicked";
-    if (h1.className === clickedClass) {
-        h1.className = "";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
     } else {
-        h1.className = clickedClass;
+        h1.classList.add(clickedClass);
     }
 }
 
