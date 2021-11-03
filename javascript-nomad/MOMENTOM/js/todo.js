408633db71970626f9042070d6dfa34679221039
Juhwan <ptagwhite@gmail.com>
2021-11-04 08:55:04 +0900

    ~ #7.3 Saving To Dos

--- javascript-nomad/MOMENTOM/js/todo.js
+++ javascript-nomad/MOMENTOM/js/todo.js
@@ -2,20 +2,37 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "×";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos)); // JSON.strungify() change to string
+}
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
