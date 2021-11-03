824e9bcedf49adb10e2f39fd367f531019db7fbc
Juhwan <ptagwhite@gmail.com>
2021-11-04 00:09:03 +0900

    ~ #3.6 CSS in Javascript

--- javascript-nomad/MOMENTOM/app3.js
+++ javascript-nomad/MOMENTOM/app3.js
@@ -1,43 +1,16 @@
-const h1 = document.querySelector(".hello h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 
-console.dir(h1);
-
-function handleh1Click() {
-    h1.style.color = "blue";
-}
-
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD");
+function handleH1Click() {
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
 // h1.onclick = handleh1Click;과 같다.
-h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleH1Click);
