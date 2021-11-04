724f2c623301e996da822ddfc5bf9ce3781617cb
Juhwan <ptagwhite@gmail.com>
2021-11-04 13:26:02 +0900

    Update weather.js

--- javascript-nomad/MOMENTOM/js/weather.js
+++ javascript-nomad/MOMENTOM/js/weather.js
@@ -4,7 +4,7 @@ function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`; // 화씨 -> 섭씨 url 끝에 '&units=metric' 추가
-    fetch(url).then(response => response.json()).then(data => {
+    fetch(url).then((response) => response.json()).then((data) => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
