a6b47ea02886448fce961f0249d6ce27d279ef96
Juhwan <dev.juhwanlee@gmail.com>
2021-11-19 15:04:12 +0900

    🔨 Add JS home contact button

--- portfolio-dream/main.js
+++ portfolio-dream/main.js
@@ -21,7 +21,17 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+  scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const contactBtn = document.querySelector('.home__contact');
+console.log(contactBtn);
+contactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
