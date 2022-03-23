f11f49c1cd8f46b03b27f273a66d928ab297bcff
Juhwan <dev.juhwanlee@gmail.com>
2022-03-23 21:51:08 +0900

    Add comment:javascript vs react

--- react-MaximilianSchwarzmüller-udemy/section3. react basic, component practice/react-complete-guide/src/App.js
+++ react-MaximilianSchwarzmüller-udemy/section3. react basic, component practice/react-complete-guide/src/App.js
@@ -1,7 +1,12 @@
 function App() {
+  // ✔️ javascript 방식 = 명령형 접근법
+  // const para = document.createElement('p')
+  // para.textContext = 'This is also visible';
+  // document.getElementById('root').append(para);
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p> {/* react방식 */}
     </div>
   );
 }
