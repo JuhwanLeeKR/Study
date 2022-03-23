864ebe6d6fc7824071300ccb141dd76583c159c4
Juhwan <dev.juhwanlee@gmail.com>
2022-03-23 17:19:36 +0900

    Create file:Array functions

---
+++ react-MaximilianSchwarzmüller-udemy/section2. javascript-refresher/Array functions.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3];
+
+const doubleNumArray = numbers.map((num) => {
+  return num * 2;
+});
+
+console.log(numbers);
+console.log(doubleNumArray);
+
+// 다양한 array function이 있으므로 공식문서를 확인하자.
+// ex) find(), findIndex(), filter(), reduce(), concat(), slice(), splice()
+// velog에 정리하던 array 함수 다시 한번 정리하자.
