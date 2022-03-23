0fb1b7836f8bdd5f29c03a8a321e2ca6e3f2a86f
Juhwan <dev.juhwanlee@gmail.com>
2022-03-23 21:36:30 +0900

    Add comment: index js

--- react-MaximilianSchwarzmüller-udemy/section3. react basic, component practice/react-complete-guide/src/index.js
+++ react-MaximilianSchwarzmüller-udemy/section3. react basic, component practice/react-complete-guide/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; // react-dom에서 ReactDOM 객체를 임포트 한다.
 import './index.css';
-import App from './App';
+import App from './App'; // 하나인 파일은 .js를 입력하지 않는다.
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -9,7 +9,8 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); // render 메서드가 객체를 가져온다.
+// <App />: JSX 객체
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
