2d4459d3f615e75db15a82da962ab3ef9cac0c05
Juhwan <dev.juhwanlee@gmail.com>
2022-03-23 17:50:46 +0900

    Create file: begin js

---
+++ react-MaximilianSchwarzmüller-udemy/section3. react basic, component practice/Begin.js
@@ -0,0 +1,22 @@
+// 😺 Module Content😺
+/* 
+- React Core Syntax & JSX
+- Working with Components
+- Working with Data
+*/
+
+/* 
+✔️ What is component?
+
+React is a Javascript library for building user interfaces
+* React is all about "Components" *
+*/
+
+/*
+✔️ How is A Component Built?
+Component = *HTML* + CSS + *Javascript*
+
+React allows you to create re-usable and reactive components
+=> Declarative Approach
+=> Define the desired target state(s) and let React figure out the actual Javascipript Dom instructions
+*/
