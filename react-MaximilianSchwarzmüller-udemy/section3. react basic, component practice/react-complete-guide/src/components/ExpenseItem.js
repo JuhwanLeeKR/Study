25bf83274f6d230a8f9c57653fef22585f03414f
Juhwan <dev.juhwanlee@gmail.com>
2022-03-23 22:17:47 +0900

    Add css in component

--- react-MaximilianSchwarzmüller-udemy/section3. react basic, component practice/react-complete-guide/src/components/ExpenseItem.js
+++ react-MaximilianSchwarzmüller-udemy/section3. react basic, component practice/react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28th 2021</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
+    // HTML처럼 보이기는 하지만 리액트가 만든 특별한 JSX 구문이다.
   );
 }
 // 하나의 return명령어당(하나의 JSX 코드당) 하나의 루트 요소만 가져올 수 있다.
