dbc2e2856f4180bc0263fd6338add1beea428639
Juhwan <dev.juhwanlee@gmail.com>
2022-03-24 11:57:31 +0900

    Edit code: lecture answer

--- react-MaximilianSchwarzmüller-udemy/section3.react-basic,component-practice/react-complete-guide/src/App.js
+++ react-MaximilianSchwarzmüller-udemy/section3.react-basic,component-practice/react-complete-guide/src/App.js
@@ -1,6 +1,27 @@
 import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   // ✔️ javascript 방식 = 명령형 접근법
   // const para = document.createElement('p')
   // para.textContext = 'This is also visible';
@@ -9,7 +30,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses />
+      <Expenses items={expenses} />
       {/* <p>This is also visible!</p>  //react방식 */}
     </div>
   );
