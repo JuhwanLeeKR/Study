c6e3f284e6babee48d46adf2c420f6a4cd299baf
Juhwan <dev.juhwanlee@gmail.com>
2022-03-24 11:27:48 +0900

    Add code:divide component

--- react-MaximilianSchwarzmüller-udemy/section3.react-basic,component-practice/react-complete-guide/src/components/ExpenseDate.js
+++ react-MaximilianSchwarzmüller-udemy/section3.react-basic,component-practice/react-complete-guide/src/components/ExpenseDate.js
@@ -1,12 +1,14 @@
+import './ExpenseDate.css';
+
 function ExpenseDate(props) {
   const month = props.date.toLocaleString('ko-KR', { month: 'long' });
   const day = props.date.toLocaleString('ko-KR', { day: '2-digit' });
   const year = props.date.getFullYear();
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
