fc3661eb74987a7c7e6e3a729586f55722de6cf9
Juhwan <dev.juhwanlee@gmail.com>
2022-03-28 19:11:53 +0900

    Merge files: module introduction

--- react-MaximilianSchwarzmüller-udemy/Module-Introduction/module-introduction.js
+++ react-MaximilianSchwarzmüller-udemy/Module-Introduction/module-introduction.js
@@ -1,4 +1,4 @@
-// 😺 Module Content😺
+// 😺 Section3.React-basic & component-practice😺
 /* 
 - React Core Syntax & JSX
 - Working with Components
@@ -19,4 +19,17 @@ Component = *HTML* + CSS + *Javascript*
 React allows you to create re-usable and reactive components
 => Declarative Approach
 => Define the desired target state(s) and let React figure out the actual Javascipript Dom instructions
+*/
+
+// 😺 Section4.state,event-handling😺
+/* 
+- Handling Events
+- Updating the UI & Working with "State"
+- A Closer Look At Componets & State
+*/
+
+// 😺 Section5.Rendering Lists & Conditional Content😺
+/* 
+- Rendering Lists & Conditional Content
+- Working With Really Dynamic Content
 */
--- react-MaximilianSchwarzmüller-udemy/Module-Introduction/section4.state,event-handling.js
+++
@@ -1,6 +0,0 @@
-// 😺 section4.state,event-handling😺
-/* 
-- Handling Events
-- Updating the UI & Working with "State"
-- A Closer Look At Componets & State
-*/
