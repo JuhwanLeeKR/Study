5712eec58c85e060ec5df8798e40d2e96c9193f4
Juhwan <dev.juhwanlee@gmail.com>
2022-03-28 23:30:36 +0900

    Add code: conditional statement

--- react-MaximilianSchwarzmüller-udemy/react-complete-guide/src/components/Expenses/Expenses.js
+++ react-MaximilianSchwarzmüller-udemy/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import './Expenses.css';
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -16,29 +16,16 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpense.length > 0) {
-    expensesContent = filteredExpense.map((expense) => (
-      <ExpenseItem
-        key={expense.id} // index를 받을 수 있지만 권장되진 않는다(버그 위험)
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
-    <div>
+    <li>
       <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangehandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpense} />
       </Card>
-    </div>
+    </li>
   );
 }
 
